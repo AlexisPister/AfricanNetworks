018778d100fdbd3295b2ba50cc562d950a928a13
Alexis Pister <alexis.pister@hotmail.com>
2023-03-20 12:07:33 +0100

    timeline

---
+++ src/App.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from '/vite.svg'
+  import Counter from './lib/Counter.svelte'
+  import TimeLine from "./lib/TimeLine.svelte";
+</script>
+
+<main id="main">
+<!--  <div id="main">-->
+    <div>
+        <h1>East African Networks</h1>
+        <h3>Social dynamics in East Africa 1950-1960</h3>
+    </div>
+    <div>
+        <TimeLine></TimeLine>
+    </div>
+<!--    <div>dziedjzied</div>-->
+<!--    <div>dziedjzied</div>-->
+<!--  </div>-->
+
+
+</main>
+
+<style>
+  #main {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+  }
+
+</style>
